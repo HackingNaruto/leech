f4839d959ebd8e1ded301c6dce604eae75c7e1e4
HackingNaruto <hackingnaruto@users.noreply.github.com>
2026-08-02 22:30:29 +0530

    fix: Add dummy lk21 module to bypass import error on Kaggle

---
+++ lk21.py
@@ -0,0 +1,6 @@
+class Bypass:
+    def __init__(self, *args, **kwargs): pass
+    def bypass_filesIm(self, url): return url
+    def bypass_fembed(self, url): return url
+    def bypass_sbembed(self, url): return url
+    def bypass_antfiles(self, url): return url
