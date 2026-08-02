9e44b895fda484ad47b2bac3a68b5b7fc8ab2181
HackingNaruto <hackingnaruto@users.noreply.github.com>
2026-08-02 22:28:19 +0530

    fix: Add dummy mega.py to bypass megasdk import error in non-docker environments

---
+++ mega.py
@@ -0,0 +1,7 @@
+class MegaApi:
+    def __init__(self, *args, **kwargs): pass
+    def getVersion(self): return "1.0.0"
+class MegaListener: pass
+class MegaRequest: pass
+class MegaTransfer: pass
+class MegaError: pass
