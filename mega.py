de7a4292b770de0de997337df95cde9ed53a9d68
HackingNaruto <hackingnaruto@users.noreply.github.com>
2026-08-02 22:32:56 +0530

    fix: Add missing MegaRequest constants to dummy mega module

--- mega.py
+++ mega.py
@@ -1,7 +1,13 @@
 class MegaApi:
     def __init__(self, *args, **kwargs): pass
     def getVersion(self): return "1.0.0"
+
 class MegaListener: pass
-class MegaRequest: pass
+
+class MegaRequest:
+    TYPE_LOGIN = 1
+    TYPE_FETCH_NODES = 2
+    TYPE_GET_PUBLIC_NODE = 3
+
 class MegaTransfer: pass
 class MegaError: pass
